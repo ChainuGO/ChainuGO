3733ccbda152404878930078ee62fdb98e37390e
chainugo <chainugo@gmail.com>
2025-12-30 16:28:01 +0800

    Update request_create_order.go

--- request_define/request_create_order.go
+++ request_define/request_create_order.go
@@ -4,4 +4,5 @@ type RequestUserCreateOrder struct {
 	ChainTokenId string `json:"ChainTokenId"`
 	OrderID      string `json:"OrderID"`
 	Amount       string `json:"Amount"`
+	ReturnUrl    string json:"ReturnUrl"
 }
